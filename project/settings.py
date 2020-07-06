4f76ecb570f1d08c2c115e0d574c2bd956d1b59c
maratdzhukaev09 <60516734+maratdzhukaev09@users.noreply.github.com>
2020-07-06 19:16:04 +0300

    Removed python-dotenv

--- project/settings.py
+++ project/settings.py
@@ -1,25 +1,23 @@
 import os
-from dotenv import load_dotenv
 from environs import Env
 
 env = Env()
 env.read_env()
-load_dotenv()
 
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'HOST': os.getenv('DB_HOST'),
-        'PORT': os.getenv('DB_PORT'),
-        'NAME': os.getenv('DB_NAME'),
-        'USER': os.getenv('DB_USER'),
-        'PASSWORD': os.getenv('DB_PASSWORD'),
+        'HOST': env('DB_HOST'),
+        'PORT': env('DB_PORT'),
+        'NAME': env('DB_NAME'),
+        'USER': env('DB_USER'),
+        'PASSWORD': env('DB_PASSWORD'),
     }
 }
 
 INSTALLED_APPS = ['datacenter']
 
-SECRET_KEY = os.getenv('SECRET_KEY')
+SECRET_KEY = env('SECRET_KEY')
 
 DEBUG = env.bool('DEBUG', False)
 
